300b73f798a2b2e46da9b02dd44d5eb5c9ba7f16
Shunsuke Yoshioka <shunsuke@fgcz-net.unizh.ch>
2024-02-08 19:14:34 +0100

    shell scripts code to execute merge blast result

---
+++ scripts/01_calc_nucleotide_diversity/merge_blast_result.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+DATA_DIR="../../data/blast_result"
+
+# nucleotide level comparison
+python merge_blast_result.py --name_A "camara" \
+                             --name_B "crivularis" \
+                             --path_ab "${DATA_DIR}/camara_vs_crivularis_nucl.txt" \
+                             --path_ba "${DATA_DIR}/crivularis_vs_camara_nucl.txt" \
+                             --out_summary "${DATA_DIR}/divergence_camara_crivularis_nucl.txt" \
+                             --out_rec_best_hit "${DATA_DIR}/reciprocal_best_hit_camara_crivularis_nucl.txt"
+
+# protein level comparison
+python merge_blast_result.py --name_A "camara" \
+                             --name_B "crivularis" \
+                             --path_ab "${DATA_DIR}/camara_vs_crivularis_prot.txt" \
+                             --path_ba "${DATA_DIR}/crivularis_vs_camara_prot.txt" \
+                             --out_summary "${DATA_DIR}/divergence_camara_crivularis_prot.txt" \
+                             --out_rec_best_hit "${DATA_DIR}/reciprocal_best_hit_camara_crivularis_prot.txt"
